90744bdfc2c415f1c43e3b3fc510dab2701c7c93
Shakil <shakil.ahmed@insidemaps.com>
2020-05-23 14:04:22 +0600

    Updates

---
+++ precache-manifest.ef8e1c03aef032e47aa0b1229561a3fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64f084197851011d81ef1b03359c900c",
+    "url": "/inside/index.html"
+  },
+  {
+    "revision": "f6116eeddcd1957361a3",
+    "url": "/inside/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3a548ad46ce8020b3500",
+    "url": "/inside/static/js/2.7ca38c6a.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/inside/static/js/2.7ca38c6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6116eeddcd1957361a3",
+    "url": "/inside/static/js/main.ec96f49c.chunk.js"
+  },
+  {
+    "revision": "0e9d91de63cfd80de8e3",
+    "url": "/inside/static/js/runtime-main.bb89aa3d.js"
+  }
+]);
